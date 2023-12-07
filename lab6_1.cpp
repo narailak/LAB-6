cd13b28c56423eb48752f78c63a7c983d50bb326
OATA <narailak_s@cmu.ac.th>
2023-12-07 23:18:28 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int n, a = 0 , b=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+    while (n != 0)
+    {
+        if (n%2 == 0)
+        {
+         a++;
+        }else{
+        b++;
+        }
+        cout << "Enter an integer: ";
+        cin >> n;
+    }
+    
+    cout << "#Even numbers = " << a << endl ;
+    cout << "#Odd numbers = " << b;
+
     return 0;
 }
